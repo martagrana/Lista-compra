9891705aa0654e6dfdd5a25d846dc7db105481a1
Marta Grana <marta.grana.fdez@gmail.com>
2023-11-25 11:53:29 +0100

    angular material

--- listaCompra/src/app/app.module.ts
+++ listaCompra/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, BrowserAnimationsModule, MatSlideToggleModule, MatButtonModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
